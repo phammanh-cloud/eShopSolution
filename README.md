e9c4cd3e55417085c93ae5c3d484db2fa2b38348
phammanh-cloud <ducmanhdv@gmail.com>
2020-10-28 15:05:50 +0700

    Command data

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 ## Techologies
 - ASP.NET Core 3.1
 - Entity Framework Core 3.1
+##Install Packet
+-Microsoft.EntityFrameWorkCore.SqlServer
+-Microsoft.EntityFrameWorkCore.Tools
+-Microsoft.EntityFrameWorkCore.Design
 ## Youtube tutorial
 ## How to configure and run
 ## How to contribute
---
+++ eShopSolution.Data/EF/eShopDbContext.cs
@@ -0,0 +1,26 @@
+﻿using eShopSolution.Data.Configuration;
+using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class eShopDbContext : DbContext
+    {
+        public eShopDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new HocSinhConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<HocSinh> HocSinhs { set; get; }
+    }
+}
